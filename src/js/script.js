ab872d69e93fb98a0ddba31bd77439128aabe14c
Vadim <vadim.zinchenco@mail.ru>
2022-10-31 01:11:57 +0400

    smooth changes

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    //Hamburger
+    const hamburger = document.querySelector(".hamburger"),
+        menu = document.querySelector(".menu"),
+        close = document.querySelector(".menu__close");
+
+    hamburger.addEventListener("click", () => {
+        menu.classList.add("active");
+    });
+    close.addEventListener("click", () => {
+        menu.classList.remove("active");
+    });
+
+
+}); //---> DOMContentLoaded
